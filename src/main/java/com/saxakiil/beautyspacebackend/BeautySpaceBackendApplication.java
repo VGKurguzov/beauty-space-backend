42fe31159e3b5062260c78e26cd5c50c04da9716
saxakiil <vovakurguzov1999@yandex.ru>
2022-06-14 23:27:50 +0300

    Init project

---
+++ src/main/java/com/saxakiil/beautyspacebackend/BeautySpaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saxakiil.beautyspacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySpaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySpaceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saxakiil/beautyspacebackend/BeautySpaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saxakiil.beautyspacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySpaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
